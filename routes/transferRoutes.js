e6c3ff944157e130cbe3a2d8bb668174893e50d3
Dhanielgeek <dhanielknightz46@gmail.com>
2026-06-01 15:19:59 +0100

    Made new changes on the Transfercontroller...

--- routes/transferRoutes.js
+++ routes/transferRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const transferController = require("../Controllers/transfer.controller");
+const transferController = require("../Controllers/transfercontroller");
 const { authenticateToken } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
@@ -11,6 +11,8 @@ router.post("/account-lookup", transferController.accountLookup);
 router.post("/internal", transferController.internalTransfer);
 router.post("/external-bank", transferController.externalBankTransfer);
 router.post("/withdrawal", transferController.withdrawal);
+router.post("/initiate-payment", transferController.initiateTransferPayment);
+
 router.post("/verify", transferController.verifyExternalTransfer);
 
 module.exports = router;
